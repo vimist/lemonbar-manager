71d9c1d4bfbe46fe254c924a94f815fa4106604d
vimist <vimist@users.noreply.github.com>
2021-04-03 12:58:14 +0100

    Reformated string concatenation to use format strings

--- examples/example_001.py
+++ examples/example_001.py
@@ -232,7 +232,7 @@ class BSPWM(Module):
             return
 
         desktop = event[event.rindex('_')+1:]
-        sp.Popen(['bspc', 'desktop', '--focus', desktop + '.local'])
+        sp.Popen(['bspc', 'desktop', '--focus', '{}.local'.format(desktop)])
 
 
 # Define the modules to put on the bar (in order)
